718210ee929f3e560e072ee5b5f79d5bc6048477
MartinX <martinx2096@gmail.com>
2026-06-13 20:49:48 +0800

    Initial commit for SoC parts (Wildfire Kernel + Custom Drivers)

--- make_deb.sh
+++ make_deb.sh
@@ -1,14 +1,19 @@
 deb_distro=bionic
 DISTRO=stable
+
+TOOLCHAIN=/home/martinx/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf                             
+export PATH=${TOOLCHAIN}/bin:${PATH} 
+
 build_opts="-j 16"
 build_opts="${build_opts} O=build_image/build"
 build_opts="${build_opts} ARCH=arm"
-build_opts="${build_opts} KBUILD_DEBARCH=${DEBARCH}"
+build_opts="${build_opts} KBUILD_DEBARCH=armhf"
 build_opts="${build_opts} LOCALVERSION=-imx6"
 build_opts="${build_opts} KDEB_CHANGELOG_DIST=${deb_distro}"
 build_opts="${build_opts} KDEB_PKGVERSION=1.$(date +%g%m)${DISTRO}"
-build_opts="${build_opts} CROSS_COMPILE=arm-linux-gnueabihf-" 	
+build_opts="${build_opts} CROSS_COMPILE=${TOOLCHAIN}/bin/arm-linux-gnueabihf-"
 build_opts="${build_opts} KDEB_SOURCENAME=linux-upstream"
+build_opts="${build_opts} HOSTCFLAGS=-fcommon"
 make ${build_opts}  npi_v7_defconfig
 make ${build_opts}  
 make ${build_opts}  bindeb-pkg
